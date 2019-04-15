d04281d1c21a95404357697dd5171471a2e3673c
Сафин Рустам (Safin_RF) <Safin_RF@directum.ru>
2019-04-15 14:08:39 +0500

    Initial commit

---
+++ ExpandRegions.Vsix/ExpandRegionsToolProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Outlining;
+using Microsoft.VisualStudio.Utilities;
+
+namespace ExpandRegionsNext.Vsix
+{
+    /// <summary>
+    /// Region expand provider.
+    /// </summary>
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [ContentType("CSharp")]
+    [ContentType("Basic")]
+    [UserVisible(true)]
+    internal class ExpandRegionsToolProvider : IWpfTextViewCreationListener
+    {
+        [Import(typeof(IOutliningManagerService))]
+        public IOutliningManagerService OutliningManagerService
+        {
+            get;
+            set;
+        }
+
+        #region IWpfTextViewCreationListener
+
+        public void TextViewCreated(IWpfTextView host)
+        {
+            if (host == null || OutliningManagerService == null)
+            {
+                return;
+            }
+
+            RegionTextViewHandler.CreateHandler(host, OutliningManagerService);
+        }
+
+        #endregion
+    }
+}
---
+++ ExpandRegions.Vsix/RegionTextViewHandler.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Outlining;
+
+namespace ExpandRegionsNext.Vsix
+{
+    internal class RegionTextViewHandler
+    {
+        #region Private Fields
+
+        private IWpfTextView _textView;
+        private IOutliningManager _outliningManager;
+
+        #endregion
+
+        #region Initialization
+
+        private RegionTextViewHandler(IWpfTextView textView, IOutliningManagerService outliningManagerService)
+        {
+            _outliningManager = outliningManagerService.GetOutliningManager(textView);
+
+            if (_outliningManager == null)
+            {
+                return;
+            }
+
+            _textView = textView;
+            _outliningManager.RegionsCollapsed += OnRegionsCollapsed;
+            _textView.Closed += OnClosed;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+        public static void CreateHandler(IWpfTextView textView, IOutliningManagerService outliningManagerService)
+        {
+            new RegionTextViewHandler(textView, outliningManagerService);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            if (_outliningManager != null)
+            {
+                _outliningManager.RegionsCollapsed -= OnRegionsCollapsed;
+            }
+
+            if (_textView != null)
+            {
+                _textView.Closed -= OnClosed;
+            }
+
+            _textView = null;
+            _outliningManager = null;
+        }
+
+        private void OnRegionsCollapsed(object sender, RegionsCollapsedEventArgs e)
+        {
+            foreach (ICollapsed collapsed in e.CollapsedRegions)
+            {
+                try
+                {
+                    _outliningManager.Expand(collapsed);
+                }
+                catch (InvalidOperationException)
+                {
+                }
+            }
+
+            _outliningManager.RegionsCollapsed -= OnRegionsCollapsed;
+        }
+
+        #endregion
+    }
+}
